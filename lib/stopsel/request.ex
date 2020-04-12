e70a29c5c34a6cd204a73341386e18e640f34184
Alex <>
2020-04-12 21:19:07 +0200

    Remove message fields

--- lib/stopsel/request.ex
+++ lib/stopsel/request.ex
@@ -4,10 +4,6 @@ defmodule Stopsel.Request do
           derived_content: String.t(),
           dispatcher: struct,
           current_command: Stopsel.Command.t(),
-          message_id: term,
-          channel_id: term,
-          server_id: term,
-          user_id: term,
           assigns: map,
           halted?: boolean
         }
@@ -16,10 +12,6 @@ defmodule Stopsel.Request do
             derived_content: nil,
             dispatcher: nil,
             current_command: nil,
-            message_id: nil,
-            channel_id: nil,
-            server_id: nil,
-            user_id: nil,
             assigns: %{},
             halted?: false
 
