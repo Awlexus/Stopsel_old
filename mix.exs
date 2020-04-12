b9574d9d813cca3a5909475cae66a80d1ee40e95
Alex <>
2020-04-12 21:56:12 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Stopsel.MixProject do
   def project do
     [
       app: :stopsel,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
